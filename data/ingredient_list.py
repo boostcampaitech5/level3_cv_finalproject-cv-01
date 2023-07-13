e118abd7314c6c5f168c33cedf3dea2a068bfdf7
Hyeji0828 <jangyeji0828@gmail.com>
2023-07-13 01:46:43 +0000

    list of allergy ingredients

---
+++ data/ingredient_list.py
@@ -0,0 +1,102 @@
+allergy_list = {
+    "egg" : {
+            "subtype": "dairy_product_egg",
+            "ingredient": "egg"
+            },
+    "beef" : {
+            "subtype": "meat",
+            "ingredient": "beef"
+            },
+    "pork" : {
+            "subtype": "meat",
+            "ingredient": "pork"
+            },
+    "chicken" : {
+            "subtype": "meat",
+            "ingredient": "chicken"
+            },
+    "shrimp" : {
+            "subtype": "sea_products",
+            "ingredient": "shrimp"
+            },
+    "tomato" : {
+            "subtype": "vegetable",
+            "ingredient": "tomato"
+            },
+    "crab" : {
+            "subtype": "sea_products",
+            "ingredient": "crab"
+            },
+    "squid_octopus" : {
+            "subtype": "sea_products",
+            "ingredient": "squid_octopus"
+            },
+    "shellfish" : {
+            "subtype": "sea_products",
+            "ingredient": "shellfish"
+            },
+    "milk" : {
+            "subtype": "dairy_product_egg",
+            "ingredient": "milk"
+            },
+    "wheat_product" : {
+            "subtype": "grain_nuts",
+            "ingredient": "wheat_products"
+            },
+    "wheat_products" : {
+            "subtype": "grain_nuts",
+            "ingredient": "wheat_products"
+            },
+    "peanut" : {
+            "subtype": "grain_nuts",
+            "ingredient": "peanut"
+            },
+    "walnut" : {
+            "subtype": "grain_nuts",
+            "ingredient": "walnut"
+            },
+    "pine_nut" : {
+            "subtype": "grain_nuts",
+            "ingredient": "pine_nut"
+            },
+    "soy" : {
+            "subtype": "bean_wasabi",
+            "ingredient": "soy"
+            },
+    "peach" : {
+            "subtype": "fruit",
+            "ingredient": "peach"
+            },
+    "buckwheat" : {
+            "subtype": "grain_nuts",
+            "ingredient": "buckwheat"
+            },
+}
+
+all_ingredients= {
+    "other_sauce_and_spices" : ["soy", "wheat_product", "egg"],
+    "other_sauce_and_spices_set_aside" : ["soy", "wheat_product", "egg"],
+    "other_sauce_and_spices_poured_into_food" : ["soy", "wheat_product", "egg"],
+    "bread" : ["wheat_products", "milk", "egg"],
+    "cheese" : ["milk"],
+    "cream" : ["milk"],
+    "processed meat" : ["pork"],
+    "chocolate" : ["milk"],
+    "wheat_noodle" : ["wheat_products"],
+    "ice_cream" : ["milk"],
+    "tofu" : ["soy"],
+    "cream_sauce" : ["milk", "soy", "wheat_products", "egg"],
+    "pasta_noodle" : ["wheat_product"],
+    "tomato_sauce" : ["tomato", "soy", "wheat_products"],
+    "mayonnaise" : ["soy", "egg"],
+    "fish_cake" : ["wheat_product", "soy", "crab", "squid_octopus", "shrimp"],
+    "butter" : ["milk"],
+    "mixed_meat" : ["pork", "beef", "chicken"],
+    "snail" : ["shellfish"],
+    "pudding" : ["egg", "milk"],
+    "ketchup" : ["tomato"],
+    "mustard" : ["soy", "egg", "milk"],
+    "milk_into_food" : ["milk"],
+    "yogurt" : ["milk"],
+    "milk_set_aside" : ["milk"],
+}
